d1ff446a20a20db019d28cdcb5fa5b5a45779839
iam-dante <koreanhoseon@gmail.com>
2020-03-18 17:24:29 +0900

    ALLOWED_HOSTS add pythonanywhere.com

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7j@718xlp_&yyr7f9wsbklby1y9bfgit&d4%r7dhsd-dv2w!w2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
 
 # Application definition
